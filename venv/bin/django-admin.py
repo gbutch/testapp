e5f8cc1ea5c51ca6a72835d188ff9e1285850b0a
gbutch <gbutcher@thoughtworks.com>
2015-04-22 14:50:11 +0100

    delete venv on heroku

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/gbutcher/Documents/coding_and_technical_resources/python-practice/projects/testapp/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
